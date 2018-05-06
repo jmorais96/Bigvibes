a1c3af1dcb7af5d3009a221c5fbc3c53c71fdebf
José Morais <jose.morais.96@hotmail.com>
2018-05-06 20:37:03 +0000

    hover artists info

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  $(".info-container").hide();
 
   // let data = new Date();
   // let hour = currentdate.getHours();
@@ -9,7 +10,7 @@ $(document).ready(function(){
   //   $("html").css("background-color", "grey");
   // }
 
-    
+
     /* HOVER DO MENU */
 
   $(".menu a").hover(function(){
@@ -20,37 +21,63 @@ $(document).ready(function(){
       $(this).css("background-color", "#fff");
       $(this).css("color", "#74C8D2");
   });
-    
-    
-    
+
+  // $(".sub-menu").hover(function(){
+  //     $(this).css("z-index", "1");
+  //     $(this).css("opacity" , "1");
+  //     }, function(){
+  //       $(this).css("z-index", "-1");
+  //       $(this).css("opacity" , "0");
+  // });
+
+
+
+
     /* HOVER DO SUBMENU */
-    
-     $(".submenu li a", "sub-menu .current-item a").hover(function(){
-      $(this).css("background-color", "#74C8D2");
-      $(this).css("color", "#fff");
-      $(this).css("transition", "all .5s ease-in-out");
-      }, function(){
-      $(this).css("background-color", "#fff");
-      $(this).css("color", "#74C8D2");
-  });
-    
-    
-    
+
+  //    $(".submenu li a", "sub-menu .current-item a").hover(function(){
+  //     $(this).css("background-color", "#74C8D2");
+  //     $(this).css("color", "#fff");
+  //     $(this).css("transition", "all .5s ease-in-out");
+  //     }, function(){
+  //     $(this).css("background-color", "#fff");
+  //     $(this).css("color", "#74C8D2");
+  // });
+
+
+
     /* HOVER DO ARTISTS */
 
-  $(".artist-photo img").hover(function(){
-      $(this).css("opacity", "1");
-      $(this).css("-webkit-transform", "scale(1.03) skewX(10deg)");
-      $(this).css("transform", "scale(1.03) skewX(10deg)");
-      $(this).css("filter", "contrast(20%)");
+  $(".artist-photo").hover(function(){
+      $(this).find("img").css("opacity", "1");
+      $(this).find("img").css("-webkit-transform", "scale(1.03) skewX(10deg)");
+      $(this).find("img").css("transform", "scale(1.03) skewX(10deg)");
+      $(this).find("img").css("filter", "contrast(20%)");
+      $(this).find(".info-container").show();
       }, function(){
-        $(this).css("opacity", "0.7");
-        $(this).css("-webkit-transform", "scale(1.18) skewX(10deg);");
-        $(this).css("transform", "skewX(10deg)");
-        $(this).css("filter", "contrast(100%)");
+        $(this).find("img").css("opacity", "0.7");
+        $(this).find("img").css("-webkit-transform", "scale(1.18) skewX(10deg);");
+        $(this).find("img").css("transform", "skewX(10deg)");
+        $(this).find("img").css("filter", "contrast(100%)");
+          $(this).find(".info-container").hide();
   });
-    
+
+  $(".info-container h4").hover(function(){
+    $(this).css("color", "black");
+    $(this).css("transition", "background-color 0.5s ease-in");
+      }, function(){
+        $(this).css("color", "white");
+        $(this).css("transition", "background-color 0.5s ease-in");
+  });
+
+  $(".info-container p").hover(function(){
+    $(this).css("color", "black");
+    //$(this).css("transition", "background-color 0.5s ease-in");
+      }, function(){
+        $(this).css("color", "white");
+        //$(this).css("transition", "background-color 0.5s ease-in");
+  });
+
 
 
 });
-
