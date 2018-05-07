f6369dc6028b9bd8412001fbbc1577b82628571a
nelson <nelsonpacheco95@gmail.com>
2018-05-07 21:05:29 +0000

    Estrutura feita 1

--- js/main.js
+++ js/main.js
@@ -24,30 +24,49 @@ $(document).ready(function(){
     
     
     /* HOVER DO SUBMENU */
-    
-     $(".submenu li a", "sub-menu .current-item a").hover(function(){
-      $(this).css("background-color", "#74C8D2");
-      $(this).css("color", "#fff");
-      $(this).css("transition", "all .5s ease-in-out");
-      }, function(){
-      $(this).css("background-color", "#fff");
-      $(this).css("color", "#74C8D2");
-  });
+    $(".menu li").hover(function(){
+      $(this).find(".sub-menu").css("transition" , "all .5s ease-in-out");
+      $(this).find(".sub-menu").css("z-index", "1");
+      $(this).find(".sub-menu").css("opacity" , "1");
+      $(this).find(".sub-menu").css("display", "block");
+    }, function(){
+      $(this).find(".sub-menu").css("z-index", "-1");
+      $(this).find(".sub-menu").css("opacity" , "0");
+      $(this).find(".sub-menu").css("display", "none");
+    });
     
     
     
     /* HOVER DO ARTISTS */
 
-  $(".artist-photo img").hover(function(){
-      $(this).css("opacity", "1");
-      $(this).css("-webkit-transform", "scale(1.03) skewX(10deg)");
-      $(this).css("transform", "scale(1.03) skewX(10deg)");
-      $(this).css("filter", "contrast(20%)");
+  $(".artist-photo").hover(function(){
+      $(this).find("img").css("opacity", "1");
+      $(this).find("img").css("-webkit-transform", "scale(1.03) skewX(10deg)");
+      $(this).find("img").css("transform", "scale(1.03) skewX(10deg)");
+      $(this).find("img").css("filter", "contrast(90%)");
+      $(this).find(".info-container").show();
       }, function(){
-        $(this).css("opacity", "0.7");
-        $(this).css("-webkit-transform", "scale(1.18) skewX(10deg);");
-        $(this).css("transform", "skewX(10deg)");
-        $(this).css("filter", "contrast(100%)");
+        $(this).find("img").css("opacity", "0.7");
+        $(this).find("img").css("-webkit-transform", "scale(1.18) skewX(10deg);");
+        $(this).find("img").css("transform", "skewX(10deg)");
+        $(this).find("img").css("filter", "contrast(100%)");
+          $(this).find(".info-container").hide();
+  });
+
+  $(".info-container h4").hover(function(){
+    $(this).css("color", "black");
+    $(this).css("transition", "background-color 0.5s ease-in");
+      }, function(){
+        $(this).css("color", "white");
+        $(this).css("transition", "background-color 0.5s ease-in");
+  });
+
+  $(".info-container p").hover(function(){
+    $(this).css("color", "black");
+    $(this).css("transition", "background-color 0.5s ease-in");
+      }, function(){
+        $(this).css("color", "white");
+        $(this).css("transition", "background-color 0.5s ease-in");
   });
     
     
