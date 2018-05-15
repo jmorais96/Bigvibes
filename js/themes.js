796513bcfd648f533ba116c5980d70d66b0de0de
nelson <nelsonpacheco95@gmail.com>
2018-05-15 12:00:17 +0000

    Hover e themes

---
+++ js/themes.js
@@ -0,0 +1,252 @@
+/*-------------------------------------------------------------*/
+                        /* COLOR SCHEME */
+/*--------------------------------------------------------------*/
+
+/* sunrise */
+
+    /* header hero img */
+     $("#sunrise").click(function(){
+         color="sunrise";
+         $("header").css("background-image", "linear-gradient(rgba(116, 200, 210, 0.7), rgba(0, 0, 0, 0)), url(img/hero1.jpg");
+         $("h2").css("color", "#74C8D2");
+         $(".btn").css("background-color", "#74C8D2");
+        
+         $(".steps div").css("background-color", "#74C8D2");
+         $(".search-field").css("border", "2px solid #74C8D2");
+         $(".contact-field").css("border", "2px solid #74C8D2");
+         $(".send-field").css("border", "2px solid #74C8D2");
+         //$(".btn").css("background-color", "#74C8D2");
+         //$(".btn:hover").css("background-color", "#000");
+         $(".social-links li a:link").css("color", "#74C8D2");
+
+
+         /* FALTA FAZER OS HOVERS DOS BOTOES */
+
+
+     });
+
+
+    /*-------------------------------------------------*/
+
+
+
+
+
+
+
+    /* midday */
+
+    /* header hero img */
+     $("#mday").click(function(){
+         color="miday";
+         $("header").css("background-image", "linear-gradient(rgba(0, 129, 198, 0.7), rgba(0, 0, 0, 0)), url(img/hero1.jpg)");
+         $("h2").css("color", "#0081C6");
+         $("h2:after").css("color", "#0081C6");
+         $(".steps div").css("background-color", "#0081C6");
+         $(".search-field").css("border", "2px solid #0081C6");
+         $(".contact-field").css("border", "2px solid #0081C6");
+         $(".send-field").css("border", "2px solid #0081C6");
+         //$(".btn").css("background-color", "#0081C6");
+         $(".social-links li a:link").css("color", "#0081C6");
+
+         /* Custom btn (botao de enviar musica) hover   FALTA FAZER */
+
+         //$("#custombtn:hover").css("background-color", "#0081C6");
+
+     });
+
+
+
+
+
+
+
+
+    /*-------------------------------------------------*/
+
+
+
+
+
+
+
+    /* sunset */
+
+    /* header hero img */
+     $("#sunset").click(function(){
+         color="sunset";
+         $("header").css("background-image", "linear-gradient(rgba(229, 92, 58, 0.7), rgba(0, 0, 0, 0)), url(img/hero1.jpg)");
+
+
+         /* Cor do titulo */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $("h2").css("color", "#E55C3A");
+     });
+
+
+    /* Cor do titulo */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $("h2:after").css("color", "#E55C3A");
+     });
+
+
+
+    /* Steps container number */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $(".steps div").css("background-color", "#E55C3A");
+     });
+
+
+
+     /* Search field border */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $(".search-field").css("border", "2px solid #E55C3A");
+     });
+
+
+
+
+         /* Contact field border */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $(".contact-field").css("border", "2px solid #E55C3A");
+     });
+
+
+
+         /* Send field border */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $(".send-field").css("border", "2px solid #E55C3A");
+     });
+
+
+
+         /* Custom btn */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $("#custombtn").css("background-color", "#E55C3A");
+     });
+
+
+
+
+
+         /* Custom btn (botao de enviar musica) hover   FALTA FAZER */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $("#custombtn:hover").css("background-color", "#E55C3A");
+     });
+
+
+
+
+         /* footer links */
+    $("#sunset").click(function(){
+         color="sunrise";
+         $(".social-links li a:link").css("color", "#E55C3A");
+     });
+
+
+         /* FALTA FAZER OS HOVERS DOS BOTOES */
+
+
+     });
+
+
+
+
+
+    /*-----------------------------------------------------------*/
+
+
+
+
+    /* night */
+
+    /* header hero img */
+     $("#night").click(function(){
+         color="sunrise";
+         $("header").css("background-image", "linear-gradient(rgba(21, 35, 66, 0.7), rgba(0, 0, 0, 0)), url(../img/hero1.jpg)");
+
+
+         /* Cor do bg */
+         $("body").css("background-color", "#131721");
+
+         $("section").css("background-color", "#131721");
+
+
+
+
+
+         /* Cor do titulo */
+         $("h2").css("color", "#fff");
+
+
+
+    /* Cor do titulo */
+         $("h2:after").css("color", "#fff");
+
+
+
+
+    /* Steps container number */
+         $(".steps div").css("background-color", "#fff");
+
+
+
+
+     /* Search field border */
+         $(".search-field").css("border", "2px solid #fff");
+
+
+
+
+
+         /* Contact field border */
+         $(".contact-field").css("border", "2px solid #fff");
+
+
+
+
+         /* Send field border */
+         $(".send-field").css("border", "2px solid #fff");
+
+
+
+
+         /* Custom btn */
+         $("#custombtn").css("background-color", "#fff");
+
+
+
+
+
+
+         /* Custom btn (botao de enviar musica) hover   FALTA FAZER */
+
+
+
+
+
+         /* footer links */
+    $("#night").click(function(){
+         color="sunrise";
+         $(".social-links li a:link").css("color", "#fff");
+     });
+
+
+         /* FALTA FAZER OS HOVERS DOS BOTOES */
+
+
+     });
+
+
+
+/*-------------------------------------------------------------*/
+                    /* END OF COLOR SCHEME */
+/*--------------------------------------------------------------*/
